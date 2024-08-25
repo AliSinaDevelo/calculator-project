67030b3a7021a4613937c3a2f72b13f69e4bff0f
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-08-25 06:39:28 +0200

    is_prime helper function implemented in views defintion

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+import math
+from django.http import JsonResponse
 # Create your views here.
+
+# create helper functions
+
+def is_prime(num):
+    # checks if a number is prime
+    if num < 2:
+        return False
+    # use the algorithm to check between 2 and the square root of the number
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
