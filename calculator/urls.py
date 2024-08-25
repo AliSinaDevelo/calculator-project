534f8f51de2ce3c842fca20e13a068e966489e7f
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-08-25 07:14:04 +0200

    Update urls  to include index

--- calculator/urls.py
+++ calculator/urls.py
@@ -4,5 +4,6 @@ from .views import calculate
 # det URL routing so that the view cam be accessed from the frontend
 urlpatterns = [
     path('calculate/', calculate, name='calculate'),
+    path('', index, name='index'), 
 ]
 
